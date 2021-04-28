bec4273ab06fd304d78f5441e2a2a4918c7f7776
emmabusarello <68125242+emmabusarello@users.noreply.github.com>
2021-04-28 08:36:48 +0200

    Update rg_function.m

--- MATLAB_code/rg_function.m
+++ MATLAB_code/rg_function.m
@@ -3,7 +3,6 @@ function outputMask = rg_function(seeds, intensity_matrix, mask, threshold, mode
     [m, n, k] = size(intensity_matrix);
     outputMask = zeros(m,n,k);
     outputMask=im2uint8(outputMask);
-
     
     [seeds_x,seeds_y,seeds_z] = ind2sub(size(seeds),find(seeds == 1));
     
@@ -15,28 +14,35 @@ function outputMask = rg_function(seeds, intensity_matrix, mask, threshold, mode
     while ~seeds_q.isempty()
         point = seeds_q.pop();
         neighbors = get_neighbors(point, mode);
+        if (outputMask(point(1), point(2), point(3)) == 0)
+            intensity_point = intensity_matrix(point(1), point(2), point(3)); %intensity seed
+            outputMask(point(1), point(2), point(3)) = intensity_point;
+        else
+            intensity_point =  outputMask(point(1), point(2), point(3)); %intensity seed
+
+        end
         
-        intensity_point = intensity_matrix(point(1), point(2), point(3)); %intensity seed
-        outputMask(point(1), point(2), point(3)) = intensity_point;
-        
-        for n = 1:length(neighbors)
-            
-            x=cell2mat(neighbors(n,1));
-            y=cell2mat(neighbors(n,2));
-            z=cell2mat(neighbors(n,3));
-             
-            if (x <= 512 && y <= 512 && z <= 341 && x > 0 && y > 0 && z > 0 && mask(x,y,z) == 1)
-                
-                intensity_neig = intensity_matrix(x,y,z); %intensity neighbors
-                                
-                if  (intensity_neig <= (intensity_point + threshold) && intensity_neig >= (intensity_point - threshold) && outputMask(x,y,z) == 0)
-                    outputMask(x,y,z) = idivide((intensity_neig + intensity_point), 2);
-                    seeds_q.push([x, y, z]);            
+            for n = 1:length(neighbors)
+
+                x=cell2mat(neighbors(n,1));
+                y=cell2mat(neighbors(n,2));
+                z=cell2mat(neighbors(n,3));
+
+                if (x <= 512 && y <= 512 && z <= 341 && x > 0 && y > 0 && z > 0 && mask(x,y,z) == 1)
+
+                    intensity_neig = intensity_matrix(x,y,z); %intensity neighbors
+
+                    if  (intensity_neig <= (intensity_point + threshold) && intensity_neig >= (intensity_point - threshold) && outputMask(x,y,z) == 0)
+                        outputMask(x,y,z) = intensity_point;
+                        seeds_q.push([x, y, z]); 
+                    end
+
+
                 end
             end
-        end
     end
 end
+
 
 
 
