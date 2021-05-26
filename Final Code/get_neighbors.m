2b3a28b90e50eb3cb08de29e9741f6e74286f592
Francesco Penasa <37983733+FrancescoPenasa@users.noreply.github.com>
2021-05-26 15:29:26 +0200

    Update get_neighbors.m

--- Final Code/get_neighbors.m
+++ Final Code/get_neighbors.m
@@ -1,14 +1,16 @@
 function neighbors = get_neighbors(point, mode)
     if (mode == "26n")
-    neighbors = {point(1)-1, point(2)-1, point(3)-1;   point(1)-1, point(2)-1, point(3);   point(1)-1, point(2)-1, point(3)+1;
-                 point(1)-1, point(2), point(3)-1;     point(1)-1, point(2), point(3);     point(1)-1, point(2), point(3)+1;
-                 point(1)-1, point(2)+1, point(3)-1;   point(1)-1, point(2)+1, point(3);   point(1)-1, point(2)+1, point(3)+1;
-                 point(1), point(2)-1, point(3)-1;     point(1), point(2)-1, point(3);     point(1), point(2)-1, point(3)+1;
-                 point(1), point(2), point(3)-1;       point(1), point(2), point(3)+1;     point(1), point(2)+1, point(3)-1;
-                 point(1), point(2)+1, point(3);       point(1), point(2)+1, point(3)+1;   point(1)+1, point(2)-1, point(3)-1;
-                 point(1)+1, point(2)-1, point(3);     point(1)+1, point(2)-1, point(3)+1; point(1)+1, point(2), point(3)-1;
-                 point(1)+1, point(2), point(3);       point(1)+1, point(2), point(3)+1;   point(1)+1, point(2)+1, point(3)-1;
-                 point(1)+1, point(2)+1, point(3);     point(1)+1, point(2)+1, point(3)+1};
+    neighbors = {point(1)-1, point(2)-1, point(3)-1;        point(1)-1, point(2)-1, point(3);       point(1)-1, point(2)-1, point(3)+1;    % x-1 ; y-1 ; z+-1
+                 point(1)-1, point(2),   point(3)-1;        point(1)-1, point(2),   point(3);       point(1)-1, point(2),   point(3)+1;    % x-1 ; y   ; z+-1
+                 point(1)-1, point(2)+1, point(3)-1;        point(1)-1, point(2)+1, point(3);       point(1)-1, point(2)+1, point(3)+1;    % x-1 ; y+1 ; z+-1
+                 
+                 point(1),   point(2)-1, point(3)-1;        point(1),   point(2)-1, point(3);       point(1),   point(2)-1, point(3)+1;    % x   ; y-1 ; z+-1
+                 point(1),   point(2),   point(3)-1;                                                point(1),   point(2),   point(3)+1;    % x   ; y   ; z+-1 
+                 point(1),   point(2)+1, point(3)-1;        point(1),   point(2)+1, point(3);       point(1),   point(2)+1, point(3)+1;    % x   ; y+1 ; z+-1
+                 
+                 point(1)+1, point(2)-1, point(3)-1;        point(1)+1, point(2)-1, point(3);       point(1)+1, point(2)-1, point(3)+1;    % x+1 ; y+1 ; z+-1
+                 point(1)+1, point(2),   point(3)-1;        point(1)+1, point(2),   point(3);       point(1)+1, point(2),   point(3)+1;    % x+1 ; y+1 ; z+-1
+                 point(1)+1, point(2)+1, point(3)-1;        point(1)+1, point(2)+1, point(3);       point(1)+1, point(2)+1, point(3)+1};   % x+1 ; y+1 ; z+-1
     end
     if (mode == "6n")
             neighbors ={point(1), point(2), point(3)-1;
